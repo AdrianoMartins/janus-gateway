a1cc72afb0151d73e914c624d0b3605d35402793
Adriano Martins <adrianomartins@gmail.com>
2020-07-05 18:49:16 -0300

    docker: amplia versão da imagem debug para 2.0

--- push_docker_image.sh
+++ push_docker_image.sh
@@ -3,14 +3,14 @@
 if [[ $1 = "debug" ]]
 then
     docker tag softphone_janus_debug docker.aytytech.com/softphone_webrtc_janus:latest_debug
-    docker tag softphone_janus_debug docker.aytytech.com/softphone_webrtc_janus:v1.1_debug
+    docker tag softphone_janus_debug docker.aytytech.com/softphone_webrtc_janus:v2.0_debug
     docker login -u="aytytech" -p="ayty@2020" docker.aytytech.com
     docker push docker.aytytech.com/softphone_webrtc_janus:latest_debug
-    docker push docker.aytytech.com/softphone_webrtc_janus:v1.1_debug
+    docker push docker.aytytech.com/softphone_webrtc_janus:v2.0_debug
 else
     docker tag softphone_janus docker.aytytech.com/softphone_webrtc_janus:latest
-    docker tag softphone_janus docker.aytytech.com/softphone_webrtc_janus:v1.0
+    docker tag softphone_janus docker.aytytech.com/softphone_webrtc_janus:v2.0
     docker login -u="aytytech" -p="ayty@2020" docker.aytytech.com
     docker push docker.aytytech.com/softphone_webrtc_janus:latest
-    docker push docker.aytytech.com/softphone_webrtc_janus:v1.0
+    docker push docker.aytytech.com/softphone_webrtc_janus:v2.0
 fi

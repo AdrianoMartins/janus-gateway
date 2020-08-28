94049c7fc026732e6bf639ef5c11208f144fb431
Adriano Martins <adrianomartins@gmail.com>
2020-08-28 15:55:47 -0300

    docker: altera o domínio do docker registry

--- push_docker_image.sh
+++ push_docker_image.sh
@@ -2,15 +2,15 @@
 
 if [[ $1 = "debug" ]]
 then
-    docker tag softphone_janus_debug docker.aytytech.com/softphone_webrtc_janus:latest_debug
-    docker tag softphone_janus_debug docker.aytytech.com/softphone_webrtc_janus:v2.0_debug
-    docker login -u="aytytech" -p="ayty@2020" docker.aytytech.com
-    docker push docker.aytytech.com/softphone_webrtc_janus:latest_debug
-    docker push docker.aytytech.com/softphone_webrtc_janus:v2.0_debug
+    docker tag softphone_janus_debug download.aytytech.om/softphone_webrtc_janus:latest_debug
+    docker tag softphone_janus_debug download.aytytech.om/softphone_webrtc_janus:v2.0_debug
+    docker login -u="aytytech" -p="ayty@2020" download.aytytech.om
+    docker push download.aytytech.om/softphone_webrtc_janus:latest_debug
+    docker push download.aytytech.om/softphone_webrtc_janus:v2.0_debug
 else
-    docker tag softphone_janus docker.aytytech.com/softphone_webrtc_janus:latest
-    docker tag softphone_janus docker.aytytech.com/softphone_webrtc_janus:v2.0
-    docker login -u="aytytech" -p="ayty@2020" docker.aytytech.com
-    docker push docker.aytytech.com/softphone_webrtc_janus:latest
-    docker push docker.aytytech.com/softphone_webrtc_janus:v2.0
+    docker tag softphone_janus download.aytytech.om/softphone_webrtc_janus:latest
+    docker tag softphone_janus download.aytytech.om/softphone_webrtc_janus:v2.0
+    docker login -u="aytytech" -p="ayty@2020" download.aytytech.om
+    docker push download.aytytech.om/softphone_webrtc_janus:latest
+    docker push download.aytytech.om/softphone_webrtc_janus:v2.0
 fi

f002d59192f9140f5a1a793659cfea0af150af0e
Adriano Martins <adrianomartins@gmail.com>
2020-09-01 15:23:05 -0300

    docker: amplia versão da imagem e altera dados de autenticação para o docker registry

--- push_docker_image.sh
+++ push_docker_image.sh
@@ -2,15 +2,15 @@
 
 if [[ $1 = "debug" ]]
 then
-    docker tag softphone_janus_debug download.aytytech.om/softphone_webrtc_janus:latest_debug
-    docker tag softphone_janus_debug download.aytytech.om/softphone_webrtc_janus:v2.0_debug
-    docker login -u="aytytech" -p="ayty@2020" download.aytytech.om
-    docker push download.aytytech.om/softphone_webrtc_janus:latest_debug
-    docker push download.aytytech.om/softphone_webrtc_janus:v2.0_debug
+    docker tag softphone_janus_debug download.aytytech.com/softphone_webrtc_janus:latest_debug
+    docker tag softphone_janus_debug download.aytytech.com/softphone_webrtc_janus:v2.1_debug
+    docker login -u="admin" -p="tNyDrxFwrJ6do2Zj" download.aytytech.com
+    docker push download.aytytech.com/softphone_webrtc_janus:latest_debug
+    docker push download.aytytech.com/softphone_webrtc_janus:v2.1_debug
 else
-    docker tag softphone_janus download.aytytech.om/softphone_webrtc_janus:latest
-    docker tag softphone_janus download.aytytech.om/softphone_webrtc_janus:v2.0
-    docker login -u="aytytech" -p="ayty@2020" download.aytytech.om
-    docker push download.aytytech.om/softphone_webrtc_janus:latest
-    docker push download.aytytech.om/softphone_webrtc_janus:v2.0
+    docker tag softphone_janus download.aytytech.com/softphone_webrtc_janus:latest
+    docker tag softphone_janus download.aytytech.com/softphone_webrtc_janus:v2.1
+    docker login -u="admin" -p="tNyDrxFwrJ6do2Zj" download.aytytech.com
+    docker push download.aytytech.com/softphone_webrtc_janus:latest
+    docker push download.aytytech.com/softphone_webrtc_janus:v2.1
 fi
